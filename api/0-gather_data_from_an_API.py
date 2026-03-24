910c02503df20002b2eb68661e87d493df094704
honore-lab <h.kamana@alustudent.com>
2026-03-24 10:05:32 +0100

    Final fix for task 0

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
-"""
-Gather data from an API
-"""
+"""Gather data from an API"""
 import requests
 import sys
 
@@ -10,11 +8,11 @@ if __name__ == "__main__":
     user_id = sys.argv[1]
 
     user_url = "https://jsonplaceholder.typicode.com/users/{}".format(user_id)
-    todo_url = "https://jsonplaceholder.typicode.com/todos?userId={}".format(
+    todos_url = "https://jsonplaceholder.typicode.com/todos?userId={}".format(
         user_id)
 
     user = requests.get(user_url).json()
-    todos = requests.get(todo_url).json()
+    todos = requests.get(todos_url).json()
 
     employee_name = user.get("name")
 
@@ -22,12 +20,12 @@ if __name__ == "__main__":
     done_tasks = 0
 
     for task in todos:
-        if task.get("completed") is True:
+        if task.get("completed"):
             done_tasks += 1
 
     print("Employee {} is done with tasks({}/{}):".format(
         employee_name, done_tasks, total_tasks))
 
     for task in todos:
-        if task.get("completed") is True:
+        if task.get("completed"):
             print("\t {}".format(task.get("title")))
